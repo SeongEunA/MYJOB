5addd5bda6f4d04ecf4249ff7d0565a366c004d8
이정우 <@DESKTOP-02P9MH0>
2021-10-31 20:41:34 +0900

    회원가입창 문구표현방식수정

--- src/main/webapp/WEB-INF/views/member/join_page.jsp
+++ src/main/webapp/WEB-INF/views/member/join_page.jsp
@@ -6,7 +6,7 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
-<script type="text/javascript" src="/resources/member/js/join_page.js?ver=352"></script>
+<script type="text/javascript" src="/resources/member/js/join_page.js?ver=009"></script>
 <style type="text/css">
 .containerDiv{
 	width: 700px;
@@ -16,69 +16,154 @@
 	margin-top: 30px;
 	padding: 20px;
 }
+
 .containerDiv input[type="text"]{
 	width: 70%;
 }
+
 .containerDiv input[type="password"]{
 	width: 70%;
 }
+
 .memberIdDiv{
-	width: 60%;
-	display: inline-block;
+	width: 100%;
 }
-.idCheckDiv{
-	width: 30%;
-	display: inline-block;
-}
-.selectTel1Div{
-	width: 100px;
-	display: inline-block;
-}
-.memberTel2Div{
-	width: 200px;
-	display: inline-block;
-}
-.memberTel3Div{
-	width: 200px;
-	display: inline-block;
-}
-.phoneCheckDiv{
-	width: 100px;
-	display: inline-block;
-}
-.memberEmail1Div{
-	width: 25%;
-	display: inline-block;
-}
-.memberEmail2Div{
-	width: 25%;
-	display: inline-block;
-}
-.memberAddr1Div{
-	width: 500px;
-	display: inline-block;
-}
-.searchAddrDiv{
-	width: 100px;
-	display: inline-block;
-}
-.memberAddr2Div{
-	width: 500px;
-	display: inline-block;
-}
-.memberEmail1Div{
-	width: 200px;
-	display: inline-block;
-}
-.memberEmail2Div{
-	width: 200px;
-	display: inline-block;
-}
-.emailCheckDiv{
-	width: 200px;
+
+.memberId{
+	width: 70%;
 	display: inline-block;
 }
 
+.memberId input[type="text"]{
+	width: 100%
+}
+
+.idCheck{
+	width: 25%;
+	display: inline-block;
+}
+
+.memberTelDiv{
+	width: 100%;
+}
+
+.memberTel1{
+	width: 15%;
+	display: inline-block;
+	margin-right: 40px;
+}
+
+.memberTelDiv select{
+	width: 100%;
+}
+
+.totalTel{
+	width: 70%;
+}
+
+.memberTel2{
+	width: 30%;
+	display: inline-block;
+	margin-right: 40px;
+}
+
+.memberTel2 input[type="text"]{
+	width: 100%
+}
+
+.memberTel3{
+	width: 35%;
+	display: inline-block;
+}
+
+.memberTel3 input[type="text"]{
+	width: 100%
+}
+
+.memberAddresDiv{
+	width: 100%;
+}
+
+.memberAddr1{
+	width: 70%;
+	display: inline-block;
+}
+
+.memberAddr1 input[type="text"]{
+	width: 100%
+}
+
+.searchAddr{
+	width: 25%;
+	display: inline-block;
+}
+
+.memberAddr2{
+	margin-top: 30px;
+}
+
+.memberEmailDiv{
+	width: 70%;
+}
+
+.memberEmail1{
+	width: 50%;
+	display: inline-block;
+	margin-right: 15px;
+}
+
+.memberEmail1 input[type="text"]{
+	width: 100%;
+}
+
+.memberEmail2{
+	width: 45%;
+	display: inline-block;
+}
+.memberEmail2 select{
+	width: 100%;
+}
+.join{
+	margin-top: 30px;
+	margin-bottom: 30px;
+}
+
+
+#noticeId{
+	margin-top: 7px;
+	margin-bottom: 7px;
+	font-size: 12px;
+}
+#noticeName{
+	margin-top: 7px;
+	margin-bottom: 7px;
+	font-size: 12px;
+}
+#noticePw{
+	margin-top: 7px;
+	margin-bottom: 7px;
+	font-size: 12px;
+}
+#noticeCpw{
+	margin-top: 7px;
+	margin-bottom: 7px;
+	font-size: 12px;
+}
+#noticeTel{
+	margin-top: 7px;
+	margin-bottom: 7px;
+	font-size: 12px;
+}
+#noticeAddres{
+	margin-top: 7px;
+	margin-bottom: 7px;
+	font-size: 12px;
+}
+#noticeEmail{
+	margin-top: 7px;
+	margin-bottom: 7px;
+	font-size: 12px;
+}
 </style>
 </head>
 <body>
@@ -86,37 +171,39 @@
 		<div class="containerDiv">
 			<div class="joinDiv">
 				<form action="/member/join" method="post" id="joinForm">
-					<div class="checkIdDiv" id="checkIdDiv">
+					<div class="memberIdDiv">
 						<div>ID</div>
-						<div class="memberIdDiv">
-							<input type="text" id="memberId" name="memberId" required>
+						<div>
+							<div class="memberId"><input type="text" id="memberId" name="memberId" required></div>
+							<div class="idCheck"><input type="button" id="idCheckbtn" value="중복확인" onclick="idCheck();"></div>
 						</div>
-						<div class="idCheckDiv">
-							<input type="button" id="idCheckbtn" value="중복확인" onclick="idCheck();">
-						</div>
+						<div id="noticeId">&nbsp;</div>
 					</div>
 					<div class="memberNameDiv">
 						<div>NAME</div>
 						<div>
 							<input type="text" id="memberName" name="memberName" required>
 						</div>
+						<div id="noticeName">&nbsp;</div>
 					</div>
 					<div class="memberPwDiv">
 						<div>PASSWORD</div>
 						<div>
 							<input type="password" id="memberPw" name="memberPw" required>
 						</div>
+						<div id="noticePw">&nbsp;</div>
 					</div>
 					<div class="confirmPwDiv">
 						<div>CONFIRM PASSWORD</div>
 						<div>
 							<input type="password" id="confirmPw">
 						</div>
+						<div id="noticeCpw">&nbsp;</div>
 					</div>
-					<div class="memberTelsDiv" id="memberTelsDiv">
-						<div class="selectTel1Div">
-							<div>TEL</div>
-							<div>
+					<div class="memberTelDiv">
+						<div>TEL</div>
+						<div class="totalTel">
+							<div class="memberTel1">
 								<select id="memberTel1" name="memberTels">
 									<option value="010" selected>010</option>
 									<option value="011">011</option>
@@ -126,35 +213,22 @@
 									<option value="019">019</option>
 								</select>
 							</div>
+							<div class="memberTel2"><input type="text" id="memberTel2" name="memberTels" required></div>
+							<div class="memberTel3"><input type="text" id="memberTel3" name="memberTels" required></div>
 						</div>
-						<div class="memberTel2Div">
-							<input type="text" id="memberTel2" name="memberTels" required>
-						</div>
-						<div class="memberTel3Div">
-							<input type="text" id="memberTel3" name="memberTels" required>
-						</div>
-						<div class="phoneCheckDiv">
-							<input type="button" id="phoneCheck" value="휴대폰인증">
-						</div>
+							<div id="noticeTel">&nbsp;</div>
 					</div>
 					<div class="memberAddresDiv">
-						<div class="memberAddr1Div">
-							<div>ADDRESS</div>
-							<input type="text" id=memberAddr1 name="memberAddres" readonly placeholder="도로명/지번 주소">
-						</div>
-						<div class="searchAddrDiv">
-							<button type="button" onclick="openPostCode();" id="memberAddrBtn">주소검색</button>
-						</div>
-						<div class="memberAddr2Div">
-							<input type="text" id="memberAddr2" name="memberAddres" placeholder="상세 주소">
-						</div>
+						<div>ADDRESS</div>
+						<div class="memberAddr1"><input type="text" id=memberAddr1 name="memberAddres" readonly placeholder="도로명/지번 주소"></div>
+						<div class="searchAddr"><button type="button" onclick="openPostCode();" id="memberAddrBtn">주소검색</button></div>
+						<div class="memberAddr2"><input type="text" id="memberAddr2" name="memberAddres" placeholder="상세 주소"></div>
+						<div id="noticeAddres">&nbsp;</div>
 					</div>
-					<div class="memberEmailDiv" id="memberEmailDiv">
-						<div class="memberEmail1Div">
-							<div>EMAIL</div>
-							<input type="text" id="memberEmail1" name="memberEmails">
-						</div>
-						<div class="memberEmail2Div">
+					<div class="memberEmailDiv">
+						<div>EMAIL</div>
+						<div class="memberEmail1"><input type="text" id="memberEmail1" name="memberEmails"></div>
+						<div class="memberEmail2">
 							<select id="memberEmail2" name="memberEmails">
 								<option value="naver.com" selected>naver.com</option>
 								<option value="daum.net">daum.net</option>
@@ -162,20 +236,16 @@
 								<option value="nate.com">nate.com</option>
 							</select>
 						</div>
-						<div class="emailCheckDiv">
-							<input type="button" id="emailCheck" value="이메일인증">
-						</div>
+						<div id="noticeEmail">&nbsp;</div>
 					</div>
-					<div>
-						<div>
-							<div>GENDER</div>
-						</div>
+					<div class="genderDiv">
+						<div>GENDER</div>
 						<div>
 							<input type="radio" name="memberGender" id="gender1" value="male" checked> 남자
 							<input type="radio" name="memberGender" id="gender2" value="female"> 여자
 						</div>
 					</div>
-					<div>
+					<div class="join">
 						<input type="submit" id="joinBtn" value="J o i n" disabled>
 					</div>
 				</form>
--- src/main/webapp/resources/member/js/join_page.js
+++ src/main/webapp/resources/member/js/join_page.js
@@ -36,7 +36,26 @@ function openPostCode(){
 $(document).ready(function(){
 	//회원가입 화면으로 오면 ID입력란에 자동포커스
 	$('#memberId').focus();
-	$('#checkIdDiv').after('<div id="insertId" style="color: red; font-size: 12px; margin-top: 7px;">아이디를 입력하세요.</div>');
+	$('#noticeId').text('아이디를 입력하세요.');
+	$('#noticeId').css('color', 'red');
+	
+	
+	//모든 정보가 제대로 입력됬을 경우에만 join버튼 활성화
+	var memberId = $('#memberId').val();
+	var memberName = $('#memberName').val();
+	var memberPw = $('#memberPw').val();
+	var confirmPw = $('#confirmPw').val();
+	var memberTel2 = $('#memberTel2').val();
+	var membertel3 = $('#memberTel3').val();
+	var memberAddr2 = $('#memberAddr2').val();
+	var memberEmail1 = $('#memberEmail1').val();
+	
+	
+	if(!memberId == '' && !memberName == '' && !memberPw == '' && !confirmPw == '' && !memberTel2 == '' && !memberTel3 == '' && !memberAddr2 == '' && memberEmail1 == ''){
+		$('#joinBtn').prop('disabled', false);
+	}
+	
+	
 	//회원가입 시 모든 데이터의 유효성 검사 진행 후 회원가입 진행//
 	
 	
@@ -52,29 +71,25 @@ $(document).ready(function(){
 		//memberName 변수 생성
 		var memberName = $('#memberName').val();
 		
-		//모든 div문구값 리셋.
-		$('#insertName').remove();
-		$('#retryInsertName').remove();
-		$('#useableName').remove();
-		$('#insertPw').remove();
-		$('#useablePw').remove();
-		$('#retryInsertPw').remove();
 		
 		//ID입력란 공백 유효성 검사 <-처음에 안먹음 이름 패스한 후 지우고 넘어가면 걸리게 됨 이상함
 		if(memberName == ''){
-			alert(memberName);
-			$('#memberName').after('<div id="insertName" style="color: red; font-size: 12px; margin-top: 7px;">이름을 입력하세요.</div>');
+			$('#noticeName').text('이름을 입력하세요.');
+			$('#noticeName').css('color', 'red');
 			$('#memberName').focus();
 		}
 		//NAME입력값 양식에 맞지 않을 때
 		else if(!nameJ.test(memberName)){
-			$('#memberName').after('<div id="retryInsertName" style="color: red; font-size: 12px; margin-top: 7px;">성을 포함한 2~7자리의 이름으로 다시 입력하세요.</div>');
+			$('#noticeName').text('성을 포함한 2~7자리의 이름으로 다시 입력하세요.');
+			$('#noticeName').css('color', 'red');
 			$('#memberName').focus();
 		}
 		//NAEM입력값 양식에 일치할 때
 		else if(nameJ.test(memberName)){
-			$('#memberName').after('<div id="useableName" style="color: green; font-size: 12px; margin-top: 7px;">사용 가능한 이름입니다.</div>');
-			$('#memberPw').after('<div id="insertPw" style="color: red; font-size: 12px; margin-top: 7px;">패스워드를 입력하세요</div>');
+			$('#noticeName').text('사용 가능한 이름입니다.');
+			$('#noticeName').css('color', 'green');
+			$('#noticePw').text('패스워드를 입력하세요');
+			$('#noticePw').css('color', 'red');
 			$('#memberPw').focus();
 		}
 	});
@@ -91,32 +106,33 @@ $(document).ready(function(){
 		//memberPw 변수 생성
 		var memberPw = $('#memberPw').val();
 		
-		//모든 div문구값 리셋.
-		$('#insertPw').remove();
-		$('#retryInsertPw').remove();
-		$('#useablePw').remove();
-		$('#cantMatchPw').remove();
-		$('#misMatchPw').remove();
-		$('#matchPw').remove();
-		$('#unuseableTels').remove();
-		$('#insertConfirmPw').remove();
-		$('#insertTels').remove();
+		//PW입력란 공백 유효성 검사
+		if(memberPw == ''){
+			$('#noticePw').text('패스워드를 입력하세요');
+			$('#noticePw').css('color', 'red');
+			$('#memberPw').focus();
 		
+		}
 		//PW입력값 양식에 맞지 않을 때
 		if(!pwJ.test(memberPw)){
-			$('#memberPw').after('<div id="retryInsertPw" style="color: red; font-size: 12px; margin-top: 7px;">영어 대소문자, 숫자로 구성된 4~12자리로 다시 입력하세요.</div>');	
+			$('#noticePw').text('영어 대소문자, 숫자로 구성된 4~12자리로 다시 입력하세요.');
+			$('#noticePw').css('color', 'red');
 			$('#memberPw').focus();
 		}
 		//PW입력값이 양식에 맞을 때
 		else{
 			//PW와 ID중복 체크 여부
 			if(memberPw == $('#memberId').val()){
-				$('#memberPw').after('<div id="cantMatchPw" style="color: red; font-size: 12px; margin-top: 7px;">PW는 ID와 일치할 수 없습니다.</div>');	
+				$('#noticePw').text('PW는 ID와 일치할 수 없습니다.');
+				$('#noticePw').css('color', 'red');
 				$('#memberPw').focus();
 			}
 			else{
-				$('#memberPw').after('<div id="useablePw" style="color: green; font-size: 12px; margin-top: 7px;">사용가능한 패스워드 입니다.</div>');
-				$('#confirmPw').after('<div id="insertConfirmPw" style="color: red; font-size: 12px; margin-top: 7px;">패스워드를 확인해주세요.</div>');
+				$('#noticePw').text('사용가능한 패스워드 입니다.');
+				$('#noticePw').css('color', 'green');
+				$('#noticeCpw').focus();
+				$('#noticeCpw').text('패스워드를 확인해주세요.');
+				$('#noticeCpw').css('color', 'red');
 				$('#confirmPw').focus();
 			}
 		}
@@ -129,24 +145,27 @@ $(document).ready(function(){
 		//confirmPw 변수 생성
 		var confirmPw = $('#confirmPw').val();
 		
-		//모든 div문구값 리셋.
-		$('#matchPw').remove();
-		$('#misMatchPw').remove();
-		$('#cantMatchPw').remove();
-		$('#retryInsertPw').remove();
-		$('#insertConfirmPw').remove();
-		$('#insertTels').remove();
-		$('#useableTels').remove();
-		$('#unuseableTels').remove();
+
+		//CONFIRM PW입력란 공백 유효성 검사
+		if(memberPw == ''){
+			$('#noticeCpw').text('패스워드를 입력하세요');
+			$('#noticeCpw').css('color', 'red');
+			$('#confirmPw').focus();
 		
-		if(memberPw == confirmPw){
-			$('#samePw').remove();
-			$('#confirmPw').after('<div id="matchPw" style="color: green; font-size: 12px; margin-top: 7px;">비밀번호가 일치합니다.</div>');
-			$('#memberTelsDiv').after('<div id="insertTels" style="color: red; font-size: 12px; margin-top: 7px;">연락처를 입력하세요.</div>');
-			$('#memberTel1').focus();
 		}
+		
+		//컨펌창 비밀번호일치
+		else if(memberPw == confirmPw){
+			$('#noticeCpw').text('비밀번호가 일치합니다.');
+			$('#noticeCpw').css('color', 'green');
+			$('#noticeTel').text('연락처를 입력하세요.');
+			$('#noticeTel').css('color', 'red');
+			$('#memberTel2').focus();
+		}
+		//컨펌창 비밀번호 불일치
 		else{
-			$('#confirmPw').after('<div id="misMatchPw" style="color: red; font-size: 12px; margin-top: 7px;">비밀번호가 일치하지 않습니다.</div>');
+			$('#noticeCpw').text('비밀번호가 일치하지 않습니다.');
+			$('#noticeCpw').css('color', 'red');
 			$('#confirmPw').focus();
 		}
 	});
@@ -160,17 +179,25 @@ $(document).ready(function(){
 		//-----------------------------------------------------------//
 		
 		
-		//membertel2 변수 생성
+		//memberTel2 변수 생성
 		var memberTel2 = $('#memberTel2').val();
-		//모든 div문구값 리셋.
-		$('#insertTels').remove();
-		$('#useableTels').remove();
-		$('#unuseableTels').remove();
 		
-		if(memberTel2 == ''|| !tel2J.test(memberTel2)){
-			$('#memberTelsDiv').after('<div id="unuseableTels" style="color: red; font-size: 12px; margin-top: 7px;">양식에 맞지 않습니다. 연락처를 다시 입력해주세요.</div>');
+		
+		//memberTel2입력란 공백 유효성 검사
+		if(memberTel2 == ''){
+			$('#noticeTel').text('연락처를 입력하세요.');
+			$('#noticeTel').css('color', 'red');
 			$('#memberTel2').focus();
 		}
+		
+		//memberTel2입력란이 양식에 맞지 않을 때
+		if(!tel2J.test(memberTel2)){
+			$('#noticeTel').text('가운데자릿수3~4자리, 끝자릿수4자리로 입력해주세요.');
+			$('#noticeTel').css('color', 'red');
+			$('#memberTel2').focus();
+		}
+		
+		//memverTel2입력란이 양식에 맞을 때
 		else{
 			$('#memberTel3').focus();
 		}
@@ -188,18 +215,27 @@ $(document).ready(function(){
 		
 		//membertel3 변수 생성
 		var membertel3 = $('#memberTel3').val();
-		//모든 div문구값 리셋.
-		$('#insertTels').remove();
-		$('#useableTels').remove();
-		$('#unuseableTels').remove();
 		
-		if(!tel3J.test(membertel3)){
-			$('#memberTelsDiv').after('<div id="unuseableTels" style="color: red; font-size: 12px; margin-top: 7px;">양식에 맞지 않습니다. 연락처를 다시 입력해주세요.</div>');
+		
+		//memberTel3입력란 공백 유효성 검사
+		if(memberTel3 == ''){
+			$('#noticeTel').text('연락처를 입력하세요.');
+			$('#noticeTel').css('color', 'red');
 			$('#memberTel3').focus();
 		}
+		
+		
+		//memberTel3입력값이 양식에 맞지 않을 때
+		if(!tel3J.test(membertel3)){
+			$('#noticeTel').text('가운데자릿수 3~4자리, 끝자릿수 4자리로 입력해주세요.');
+			$('#noticeTel').css('color', 'red');
+			$('#memberTel3').focus();
+		}
+		//memberTel3입력값이 양식에 맞을 때
 		else{
-			$('#memberTelsDiv').after('<div id="useableTels" style="color: green; font-size: 12px; margin-top: 7px;">사용 가능한 연락처 입니다.</div>');
-			$('#memberAddr1').focus();
+			$('#noticeTel').text('사용 가능한 연락처 입니다.');
+			$('#noticeTel').css('color', 'green');
+			$('#memberAddr2').focus();
 		}
 		
 	});
@@ -218,16 +254,25 @@ $(document).ready(function(){
 		//membertel3 변수 생성
 		var memberAddr2 = $('#memberAddr2').val();
 		
-		//모든 div문구값 리셋.
-		$('#useableAddr').remove();
-		$('#unuseableAddr').remove();
+		
+		//memberTel3입력란 공백 유효성 검사
+		if(memberAddr2 == ''){
+			$('#noticeAddres').text('상세주소를 입력하세요.');
+			$('#noticeAddres').css('color', 'red');
+			$('#memberAddr2').focus();
+		}
+		
 		
 		if(!addrJ.test(memberAddr2)){
-			$('#memberAddr2').after('<div id="unuseableAddr" style="color: red; font-size: 12px; margin-top: 7px;">양식에 맞지 않습니다. 주소를 다시 입력해주세요.</div>');
+			$('#noticeAddres').text('영어대소문자, 특수문자를 제외한 한글과, 띄어쓰기, 숫자의 조합으로만 입력하세요.');
+			$('#noticeAddres').css('color', 'red');
 			$('#memberAddr2').focus();
 		}
 		else{
-			$('#memberAddr2').after('<div id="useableAddr" style="color: green; font-size: 12px; margin-top: 7px;">사용 가능한 주소입니다.</div>');
+			$('#noticeAddres').text('사용 가능한 주소입니다.');
+			$('#noticeAddres').css('color', 'green');
+			$('#noticeEmail').text('이메일을 입력하세요.');
+			$('#noticeEmail').css('color', 'red');
 			$('#memberEmail1').focus();
 		}
 		
@@ -239,7 +284,7 @@ $(document).ready(function(){
 	});	
 	//------------------이메일 관련 유효성검사----------------------//
 	//7. EMAIL의 select를 고른 후 다른 노드로 전환됬을 때 memberEmail1의 유효성검사 시작
-	$(document).on('change', '#memberEmail1', function() {
+	$(document).on('change', '#memberEmail2', function() {
 		//------------------------정규표현식-------------------------//
 		
 		//이메일 검사 정규표현식
@@ -249,13 +294,17 @@ $(document).ready(function(){
 		//memberEmail1 변수 생성
 		var memberEmail1 = $('#memberEmail1').val();
 		
-		//모든 div문구값 리셋.
-		$('#useableEmail').remove();
-		$('#unuseableEmail').remove();
+		//memberEmail1입력란 공백 유효성 검사
+		if(memberEmail1 == ''){
+			$('#noticeEmail').text('이메일을 입력하세요.');
+			$('#noticeEmail').css('color', 'red');
+			$('#memberTel3').focus();
+		}
 		
 		//양식에 맞지 않을 때
 		if(!emailJ.test(memberEmail1)){
-			$('#memberEmailDiv').after('<div id="unuseableEmail" style="color: red; font-size: 12px; margin-top: 7px;">양식에 맞지 않습니다. 이메일을 다시 입력해주세요.</div>');
+			$('#noticeEmail').text('영어소문자 a~z 또는 숫자 0~9의 조합인 4~12자리로 작성하세요.');
+			$('#noticeEmail').css('color', 'red');
 			$('#memberEmail1').focus();
 		}
 		
@@ -271,14 +320,13 @@ $(document).ready(function(){
 	            	//기가입 : true, 미가입 : false
 	            	//아이디 중복 일 때.
 	            	if(result){
-						$('#memberEmailDiv').after('<div id="unuseableEmail" style="color: red; font-size: 12px; margin-top: 7px;">중복된 이메일 입니다.</div>');
+	            		$('#noticeEmail').text('중복된 이메일입니다.');
+	        			$('#noticeEmail').css('color', 'red');
 						$('#memberEmail1').focus();
 					}
 					else{
-						$('#memberEmailDiv').after('<div id="unuseableEmail" style="color: green; font-size: 12px; margin-top: 7px;">사용 가능한 이메일입니다.</div>');
-						alert(111);
-						$('#joinBtn').removeClass('disabled');
-						$('#joinBtn').prop('disabled', false);
+						$('#noticeEmail').text('사용 가능한 이메일입니다.');
+	        			$('#noticeEmail').css('color', 'green');
 					}
 	            	
 	            },
@@ -290,9 +338,7 @@ $(document).ready(function(){
 			
 			
 			
-		}
-		$('#emailCheckDiv').after('<div id="useableEmail" style="color: green; font-size: 12px; margin-top: 7px;">사용 가능한 주소입니다.</div>');
-		
+		}		
 		
 		
 		
@@ -328,27 +374,18 @@ $(document).ready(function(){
 		//memberId 변수 생성
 		var memberId = $('#memberId').val();
 		
-		//모든 div문구값 리셋.
-		$('#insertId').remove();
-		$('#joinedId').remove();
-		$('#retryInsertId').remove();
-		$('#useableId').remove();
-		$('#insertName').remove();
-		$('#retryInsertName').remove();
-		$('#useableName').remove();
-		$('#retryInsertPw').remove();
-		$('#useableTels').remove();
-		$('#unuseableTels').remove();
 		
 		//ID입력란 공백 유효성 검사
 		if(memberId == ''){
-			$('#checkIdDiv').after('<div id="insertId" style="color: red; font-size: 12px; margin-top: 7px;">아이디를 입력하세요.</div>');
+			$('#noticeId').text('아이디를 입력하세요.');
+			$('#noticeId').css('color', 'red');
 			$('#memberId').focus();
 		}
 		
 		//ID입력값 사이의 공백 유효성 검사
 		else if(empJ.test(memberId)){
-			$('#checkIdDiv').after('<div id="retryInsertId" style="color: red; font-size: 12px; margin-top: 7px;">아이디를 다시 입력하세요.</div>');
+			$('#noticeId').text('아이디를 입력하세요.');
+			$('#noticeId').css('color', 'red');
 			$('#memberId').focus();
 		}
 		
@@ -357,13 +394,13 @@ $(document).ready(function(){
 		else if(memberId != '' || !empJ.test(memberId)){
 			//ID입력값이 ID입력양식에 어긋날 때
 			if(!idJ.test(memberId)){
-				$('#checkIdDiv').after('<div id="retryInsertId" style="color: red; font-size: 12px; margin-top: 7px;">한글 또는 특수문자를 제외한 영어소문자, 숫자로만 구성된 4~12자리로 다시 입력하세요.</div>');
+				$('#noticeId').text('한글 또는 특수문자를 제외한 영어소문자, 숫자로만 구성된 4~12자리로 다시 입력하세요.');
+				$('#noticeId').css('color', 'red');
 				$('#memberId').focus();
 			}
 			//ID입력값이 ID입력양식에 일치할 때
 			else{
-				$('#insertId').remove();
-				$('#retryInsertId').remove();
+
 				
 				//ID중복체크
 				$.ajax({
@@ -374,14 +411,15 @@ $(document).ready(function(){
 		            	//기가입 : true, 미가입 : false
 		            	//아이디 중복 일 때.
 		            	if(result){
-		            		$('#retryInsertName').remove();
-							$('#checkIdDiv').after('<div id="joinedId" style="color: red; font-size: 12px; margin-top: 7px;">아이디가 중복입니다.</div>');
+		            		$('#noticeId').text('아이디가 중복입니다.');
+		    				$('#noticeId').css('color', 'red');
 							$('#memberId').focus();
 						}
 						else{
-							$('#retryInsertName').remove();
-							$('#checkIdDiv').after('<div id="useableId" style="color: green; font-size: 12px; margin-top: 7px;">사용 가능한 아이디입니다.</div>');
-							$('#memberName').after('<div id="insertName" style="color: red; font-size: 12px; margin-top: 7px;">이름을 입력하세요.</div>');
+							$('#noticeId').text('사용 가능한 아이디입니다.');
+		    				$('#noticeId').css('color', 'green');
+		    				$('#noticeName').text('이름을 입력하세요.');
+		    				$('#noticeName').css('color', 'red');
 							$('#memberName').focus();
 						}
 		            	
